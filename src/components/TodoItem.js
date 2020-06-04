b50679e99d82fbc6ca273c9c70dd4e7a318df20c
bruno moura <zbnmoura@gmail.com>
2020-06-04 14:14:49 -0300

    52:19

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const getStyle = (props) => {
+    return {
+        background: '#f4f4f4',
+        padding: '10px',
+        borderBottom: '1px #ccc dotted',
+        textDecoration: props.todo.completed ? 'line-through' : 'none',
+    };
+};
+
+const markComplete = (e) => {
+    console.log(e)
+};
+
 function TodoItem(props) {
     return (
-        <div className="App">
-            <p>{props.todo.title}</p>
+        <div style={getStyle(props)}>
+            <p>
+                <input type="checkbox" onChange={markComplete} />
+                {''}
+                {props.todo.title}
+            </p>
         </div>
     );
 }
